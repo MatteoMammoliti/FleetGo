2d711ab0380771322cd15807954530821b2f6d32
Matteo Mammoliti <matti.mm04@gmail.com>
2025-11-21 22:49:13 +0100

    pulizia repo + nuova struttura

---
+++ backend/src/test/java/it/unical/webapplications/fleetgo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unical.webapplications.fleetgo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
