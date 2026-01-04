cc1086a4484ddd963d050607029acfcbc8f5b362
AngeloVivacqua <vvcngl98d01d086i@studenti.unical.it>
2026-01-04 19:59:33 +0100

    fix set luogo veicolo

--- backend/src/main/java/it/unical/fleetgo/backend/Controller/AdminAziendale/ControllerGestioneVeicolo.java
+++ backend/src/main/java/it/unical/fleetgo/backend/Controller/AdminAziendale/ControllerGestioneVeicolo.java
@@ -3,7 +3,6 @@ package it.unical.fleetgo.backend.Controller.AdminAziendale;
 import it.unical.fleetgo.backend.Models.DTO.VeicoloDTO;
 import it.unical.fleetgo.backend.Service.VeicoloService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
