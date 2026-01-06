07cf748fe372b94253498212cf18b4c7d5aeb248
Matteo Mammoliti <matti.mm04@gmail.com>
2026-01-06 17:37:22 +0100

    Fix date + aggiunta immagini su cloud

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,13 @@
-import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import {provideRouter, withInMemoryScrolling} from '@angular/router';
+import { ApplicationConfig, LOCALE_ID, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { provideHttpClient } from '@angular/common/http';
-
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { registerLocaleData } from '@angular/common';
+import localeIt from '@angular/common/locales/it';
 import { routes } from './app.routes';
-import {provideCharts, withDefaultRegisterables} from 'ng2-charts';
+
+registerLocaleData(localeIt);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,6 +19,8 @@ export const appConfig: ApplicationConfig = {
     })),
     importProvidersFrom(FormsModule),
     provideHttpClient(),
-    provideCharts(withDefaultRegisterables())
+    provideCharts(withDefaultRegisterables()),
+
+    { provide: LOCALE_ID, useValue: 'it-IT' }
   ]
 };
