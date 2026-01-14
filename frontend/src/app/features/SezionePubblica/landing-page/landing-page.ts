efb76dd1dc24aa4812525543a48acc03c047e98e
umbyyy <umbertofrancescomessina@gmail.com>
2026-01-14 15:03:25 +0100

    banner successo/errore in contattaci

--- frontend/src/app/features/SezionePubblica/landing-page/landing-page.ts
+++ frontend/src/app/features/SezionePubblica/landing-page/landing-page.ts
@@ -5,6 +5,7 @@ import {ComeFunziona} from '../Componenti/come-funziona/come-funziona';
 import {Vantaggi} from '../Componenti/vantaggi/vantaggi';
 import {FormContatto} from '@shared/Componenti/form-contatto/form-contatto';
 import {SezionePubblicaService} from '@features/SezionePubblica/sezionePubblicaService';
+import { BannerErrore } from '@shared/Componenti/Banner/banner-errore/banner-errore';
 
 @Component({
   selector: 'app-landing-page',
@@ -13,7 +14,8 @@ import {SezionePubblicaService} from '@features/SezionePubblica/sezionePubblicaS
     ChiSiamo,
     ComeFunziona,
     Vantaggi,
-    FormContatto
+    FormContatto,
+    BannerErrore
   ],
   templateUrl: './landing-page.html',
   styleUrl: './landing-page.css',
@@ -25,25 +27,51 @@ export class LandingPage {
   }
 
   invioInCorso = false;
+  erroreInvio = false;
+  messaggioInviato = false;
+  successoBanner = '';
+  erroreBanner = '';
 
   invia(formData: FormData) {
-
     this.invioInCorso = true;
+    this.messaggioInviato = false;
+    this.erroreInvio = false;
 
     this.sezionePubblicaService.inviaForm(formData).subscribe({
       next: data => {
-
         setTimeout(() => {
           this.invioInCorso = false;
-        }, 2000)
-
-      }, error: error => {
+          this.messaggioInviato = true; 
+          this.gestisciSuccesso('Il tuo messaggio è stato inviato correttamente.');
+        }, 500);
+      },
+      error: error => {
         console.log(error);
-
         setTimeout(() => {
           this.invioInCorso = false;
-        }, 2000)
+          this.erroreInvio = true; 
+          this.gestisciErrore('Si è verificato un errore durante l\'invio del messaggio.');
+        }, 500);
       }
-    })
+    });
+  }
+
+  resetStato() {
+    this.messaggioInviato = false;
+    this.erroreInvio = false;
+    this.invioInCorso = false;
+  }
+
+
+  gestisciErrore(messaggio: string) {
+    this.successoBanner = '';
+    this.erroreBanner = messaggio;
+    setTimeout(() => this.erroreBanner = '', 5000);
+  }
+
+  gestisciSuccesso(messaggio: string) {
+    this.erroreBanner = '';
+    this.successoBanner = messaggio;
+    setTimeout(() => this.successoBanner = '', 3000);
   }
 }
